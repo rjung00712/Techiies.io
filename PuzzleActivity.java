739bfc043f011d727fda3eb743e08fdf6ea3fc5e
MagLoir <mpana3000@gmail.com>
2017-04-07 10:48:52 -0700

    Add files via upload

---
+++ PuzzleActivity.java
@@ -0,0 +1,120 @@
+package io.techies.com.puzzle_8;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+public class PuzzleActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private Bitmap imageBitmap = null;
+    private PuzzleBoardView boardView;
+
+//    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_puzzle);
+
+        // This code programmatically adds the PuzzleBoardView to the UI.
+        RelativeLayout container = (RelativeLayout) findViewById(R.id.puzzle_container);
+
+
+
+//        imageView = (ImageView) findViewById(R.id.imageView);
+
+        boardView = new PuzzleBoardView(this);
+        
+        // Some setup of the view.
+        boardView.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT));
+        container.addView(boardView);
+
+
+//        final SharedPreferences prefs = PreferenceManager
+//                .getDefaultSharedPreferences(this);
+//        String userName = prefs.getString("user_name", null);
+//        if (userName == null) {
+//            EditText input = new EditText(this);
+//            input.setId(1000);
+//            AlertDialog dialog = new AlertDialog.Builder(this)
+//                    .setView(input).setTitle("Enter your username!")
+//                    .setPositiveButton("Ok",
+//                            new DialogInterface.OnClickListener() {
+//
+//                                @Override
+//                                public void onClick(DialogInterface dialog,
+//                                                    int which) {
+//                                    EditText theInput = (EditText) ((AlertDialog) dialog)
+//                                            .findViewById(1000);
+//                                    String enteredText = theInput.getText()
+//                                            .toString();
+//                                    if (!enteredText.equals("")) {
+//                                        SharedPreferences.Editor editor = prefs
+//                                                .edit();
+//                                        editor.putString("user_name",
+//                                                enteredText);
+//                                        editor.commit();
+//                                    }
+//                                }
+//                            }).create();
+//            dialog.show();
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_puzzle, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void dispatchTakePictureIntent(View view) {  // handler for the "Take photo" button
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+
+            imageBitmap = (Bitmap) extras.get("data");
+
+            boardView.initialize(imageBitmap);
+//            imageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+    public void shuffleImage(View view) {
+        boardView.shuffle();
+    }
+
+    public void solve(View view) {
+        boardView.solve();
+    }
+}
---
+++ PuzzleBoard.java
@@ -0,0 +1,160 @@
+package io.techies.com.puzzle_8;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import java.util.ArrayList;
+
+/**
+ * Created by Richard on 4/3/17.
+ */
+
+public class PuzzleBoard {
+    private static final int NUM_TILES = 3;
+    private static final int[][] NEIGHBOUR_COORDS = {
+            { -1, 0 },
+            { 1, 0 },
+            { 0, -1 },
+            { 0, 1 }
+    };
+    private ArrayList<PuzzleTile> tiles;
+
+    private int steps;
+    private PuzzleBoard previousBoard;
+
+    PuzzleBoard(Bitmap bitmap, int parentWidth) {
+        tiles = new ArrayList<>();
+        Bitmap scaledBitmap = Bitmap.createScaledBitmap(bitmap, parentWidth, parentWidth, true);
+        int chunkSizeWidth = parentWidth / NUM_TILES;
+        int chunkSizeHeight = scaledBitmap.getHeight() / NUM_TILES;
+        int count = 0;
+        for(int x = 0; x < NUM_TILES; x++){
+            for(int y = 0; y < NUM_TILES; y++){
+                if((count) < NUM_TILES * NUM_TILES - 1) {
+                    Bitmap tile = Bitmap.createBitmap(scaledBitmap, y * chunkSizeWidth, x * chunkSizeHeight, chunkSizeWidth, chunkSizeHeight);
+                    PuzzleTile t = new PuzzleTile(tile, count);
+                    tiles.add(t);
+                } else {
+                    tiles.add(null);
+                }
+                count++;
+            }
+        }
+    }
+
+    PuzzleBoard(PuzzleBoard otherBoard) {
+        tiles = (ArrayList<PuzzleTile>) otherBoard.tiles.clone();
+
+        steps = otherBoard.steps + 1;
+        previousBoard = otherBoard;
+    }
+
+    public void reset() {
+        // Nothing for now but you may have things to reset once you implement the solver.
+        this.steps = 0;
+        this.previousBoard = null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null)
+            return false;
+        return tiles.equals(((PuzzleBoard) o).tiles);
+    }
+
+    public void draw(Canvas canvas) {
+        if (tiles == null) {
+            return;
+        }
+        for (int i = 0; i < NUM_TILES * NUM_TILES; i++) {
+            PuzzleTile tile = tiles.get(i);
+            if (tile != null) {
+                tile.draw(canvas, i % NUM_TILES, i / NUM_TILES);
+            }
+        }
+    }
+
+    public boolean click(float x, float y) {
+        for (int i = 0; i < NUM_TILES * NUM_TILES; i++) {
+            PuzzleTile tile = tiles.get(i);
+            if (tile != null) {
+                if (tile.isClicked(x, y, i % NUM_TILES, i / NUM_TILES)) {
+                    return tryMoving(i % NUM_TILES, i / NUM_TILES);
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean tryMoving(int tileX, int tileY) {
+        for (int[] delta : NEIGHBOUR_COORDS) {
+            int nullX = tileX + delta[0];
+            int nullY = tileY + delta[1];
+            if (nullX >= 0 && nullX < NUM_TILES && nullY >= 0 && nullY < NUM_TILES &&
+                    tiles.get(XYtoIndex(nullX, nullY)) == null) {
+                swapTiles(XYtoIndex(nullX, nullY), XYtoIndex(tileX, tileY));
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean resolved() {
+        for (int i = 0; i < NUM_TILES * NUM_TILES - 1; i++) {
+            PuzzleTile tile = tiles.get(i);
+            if (tile == null || tile.getNumber() != i)
+                return false;
+        }
+        return true;
+    }
+
+    private int XYtoIndex(int x, int y) {
+        return x + y * NUM_TILES;
+    }
+
+    protected void swapTiles(int i, int j) {
+        PuzzleTile temp = tiles.get(i);
+        tiles.set(i, tiles.get(j));
+        tiles.set(j, temp);
+    }
+
+    public ArrayList<PuzzleBoard> neighbors() {
+        ArrayList<PuzzleBoard> boards = new ArrayList<>();
+        PuzzleBoard t;
+        PuzzleTile nullTile = null;
+
+        int nullTileIndex = 0;
+
+        int nullR = nullTileIndex % 3;
+        int nullC = nullTileIndex / 3;
+
+        for (int[] delta : NEIGHBOUR_COORDS) {
+            int nX = nullC + delta[0];
+            int nY = nullR + delta[1];
+
+            if (nX >= 0 && nX < NUM_TILES && nY >= 0 && nY < NUM_TILES) {
+                t = new PuzzleBoard(this);
+                t.tryMoving(nX, nY);
+                boards.add(t);
+            }
+        }
+
+        return boards;
+    }
+
+    public int priority() {
+        int mPriority = 0;
+        for(int i = 0; i < NUM_TILES * NUM_TILES; i++) {
+            if(tiles.get(i) != null) {
+                int originalPlace = tiles.get(i).getNumber();
+                int x = i / NUM_TILES;
+                int y = i % NUM_TILES;
+                int xOriginal = originalPlace / NUM_TILES;
+                int yOriginal = originalPlace % NUM_TILES;
+
+                mPriority += (Math.abs(x - xOriginal) + Math.abs(y - yOriginal));
+            }
+        }
+        int manhattanDistance = mPriority + steps;
+        return manhattanDistance;
+    }
+}
